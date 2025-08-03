387b428aa5248908d5e6dad7e641e242dca730b6
Prathameshkirpal <prathameshkirpal@gmail.com>
2025-08-03 14:43:04 +0530

    added breadcrum for bbq heminway theme only

--- classes/class-breadcrumb.php
+++ classes/class-breadcrumb.php
@@ -15,72 +15,68 @@ defined( 'ABSPATH' ) || exit;
  * Class Breadcrumb
  */
 class Breadcrumb {
-		/**
-		 * Constructor.
-		 */
-	public function __construct() {
-		$this->init_output_hook();
-	}
+    /**
+     * Constructor.
+     */
+    public function __construct() {
+        add_action( 'template_redirect', array( $this, 'inject_breadcrumb_html' ) );
+    }
 
-	/**
-	 * Hook to buffer and inject breadcrumb HTML after header.
-	 */
-	public function init_output_hook() {
-		add_action( 'template_redirect', array( $this, 'start_output_buffer' ) );
-	}
+    /**
+     * Injects breadcrumb HTML after the header and before the page title.
+     */
+    public function inject_breadcrumb_html() {
+        // Don't show breadcrumb on front page or home page
+        if ( is_front_page() || is_home() ) {
+            return;
+        }
 
-	/**
-	 * Start buffering template output.
-	 */
-	public function start_output_buffer() {
-		ob_start( array( $this, 'inject_breadcrumb_html' ) );
-	}
+        // Start the breadcrumb HTML
+        $breadcrumb  = '<div class="breadcrumb-wrapper" style="padding:10px 20px;font-size:14px;">';
+        $breadcrumb .= '<a href="' . esc_url( home_url() ) . '" rel="nofollow">Home</a>';
 
-	/**
-	 * Injects breadcrumb HTML after the opening header tag.
-	 *
-	 * @param string $content Full HTML output.
-	 * @return string Modified output.
-	 */
-	public function inject_breadcrumb_html( $content ) {
-		if ( is_front_page() || is_home() ) {
-			return $content;
-		}
+        // If we're on a category page or single post, generate the breadcrumb
+        if ( is_category() || is_single() ) {
+            $breadcrumb .= '&nbsp;&nbsp;&#187;&nbsp;&nbsp;';
+            $breadcrumb .= get_the_category_list( ' &bull; ' );
 
-		$breadcrumb  = '<div class="breadcrumb-wrapper" style="padding:10px 20px;font-size:14px;">';
-		$breadcrumb .= '<a href="' . esc_url( home_url() ) . '" rel="nofollow">Home</a>';
+            if ( is_single() ) {
+                global $post;
+                $meta     = get_post_meta( $post->ID );
+                $seo_slug = isset( $meta['_seo_slug_url'][0] ) ? $meta['_seo_slug_url'][0] : '';
+                $seo_slug = str_replace( '-', ' ', $seo_slug );
 
-		if ( is_category() || is_single() ) {
-			$breadcrumb .= '&nbsp;&nbsp;&#187;&nbsp;&nbsp;';
-			$breadcrumb .= get_the_category_list( ' &bull; ' );
+                if ( ! empty( $seo_slug ) ) {
+                    $breadcrumb .= ' &nbsp;&nbsp;&#187;&nbsp;&nbsp; ';
+                    $breadcrumb .= esc_html( $seo_slug );
+                }
+            }
+        }
+        // If we're on a page
+        elseif ( is_page() ) {
+            $breadcrumb .= '&nbsp;&nbsp;&#187;&nbsp;&nbsp;';
+            $breadcrumb .= get_the_title();
+        }
+        // If we're on a search results page
+        elseif ( is_search() ) {
+            $breadcrumb .= '&nbsp;&nbsp;&#187;&nbsp;&nbsp;Search Results for "<em>' . esc_html( get_search_query() ) . '</em>"';
+        }
+        // If we're on a tag archive
+        elseif ( is_tag() ) {
+            $breadcrumb .= '&nbsp;&nbsp;&#187;&nbsp;&nbsp;<a href="' . esc_url( home_url( '/topics' ) ) . '">Topics</a> &#187;&nbsp;';
+            $breadcrumb .= esc_html( single_tag_title( '', false ) );
+        }
 
-			if ( is_single() ) {
-				global $post;
-				$meta     = get_post_meta( $post->ID );
-				$seo_slug = isset( $meta['_seo_slug_url'][0] ) ? $meta['_seo_slug_url'][0] : '';
-				$seo_slug = str_replace( '-', ' ', $seo_slug );
+        // Close the breadcrumb wrapper
+        $breadcrumb .= '</div>';
 
-				if ( ! empty( $seo_slug ) ) {
-					$breadcrumb .= ' &nbsp;&nbsp;&#187;&nbsp;&nbsp; ';
-					$breadcrumb .= esc_html( $seo_slug );
-				}
-			}
-		} elseif ( is_page() ) {
-			$breadcrumb .= '&nbsp;&nbsp;&#187;&nbsp;&nbsp;';
-			$breadcrumb .= get_the_title();
-		} elseif ( is_search() ) {
-			$breadcrumb .= '&nbsp;&nbsp;&#187;&nbsp;&nbsp;Search Results for "<em>' . esc_html( get_search_query() ) . '</em>"';
-		} elseif ( is_tag() ) {
-			$breadcrumb .= '&nbsp;&nbsp;&#187;&nbsp;&nbsp;<a href="' . esc_url( home_url( '/topics' ) ) . '">Topics</a> &#187;&nbsp;';
-			$breadcrumb .= esc_html( single_tag_title( '', false ) );
-		}
-
-		$breadcrumb .= '</div>';
-
-		// Inject breadcrumb after <header> or </header> or before <main>.
-		$content = preg_replace( '/(<\/header>)/i', $breadcrumb . '$1', $content, 1 );
-
-		return $content;
-	}
-
+        // Output the breadcrumb HTML after the header and before the content
+        add_filter( 'the_content', function( $content ) use ( $breadcrumb ) {
+            // Only add breadcrumb before content for single post or page
+            if ( is_single() || is_page() ) {
+                return $breadcrumb . $content;
+            }
+            return $content;
+        });
+    }
 }
